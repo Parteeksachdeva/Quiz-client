fa0d572d9c0da6cf34da4f9172898379e54127f8
Parteeksachdeva <parteeksachdeva900@gmail.com>
2021-08-23 10:52:48 +0530

    Fixed

--- src/axios.js
+++ src/axios.js
@@ -1,4 +1,4 @@
 import axios from "axios"
-const axios1 = axios.create({ baseURL: 'http://localhost:8000' });
+const axios1 = axios.create({ baseURL: 'https://navigus---assignment.herokuapp.com/' });
 
 export default axios1;
